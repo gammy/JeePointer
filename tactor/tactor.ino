54d9821ad2905d3b1f2eacedc73110f87eee4c38
gammy <gammy@luna>
2012-04-30 21:08:17 +0100

    Tactor: Only activate RF & transmit if new axis measurement differs from the old one. This drastically reduces power consumption when the unit isn't moving.

--- tactor/tactor.ino
+++ tactor/tactor.ino
@@ -9,29 +9,38 @@ PortI2C myBus (1);
 GravityPlug sensor (myBus);
 MilliTimer measureTimer;
 
-
 void setup () {
 
 	activity_led.digiWrite(1);
 	rf12_initialize(7, RF12_868MHZ, 5);
 	sensor.begin();
-	sensor.sensitivity(2, 25); // 2g, 25hz (lowest bandwidth, highest best signal/noise ratio)
+	sensor.sensitivity(2, 25); // 2g, 25hz (lowest bandwidth, best signal/noise ratio)
 }
 
 void loop () {
 
+	static int old[3];
+
 	if(measureTimer.poll(50)) {
 
-		const int* p = sensor.getAxes();
+		const int *current = sensor.getAxes();
 
-		rf12_sleep(RF12_WAKEUP);
+		if(current[0] == old[0] ||
+		   current[1] == old[1] ||
+		   current[2] == old[2])
+			return;
+
 		activity_led.digiWrite(1);
+		rf12_sleep(RF12_WAKEUP);
 
 		while(! rf12_canSend())
 			rf12_recvDone();
 
-		rf12_sendStart(0, p, 3 * sizeof *p, 2);
-		activity_led.digiWrite(0);
+		rf12_sendStart(0, current, 3 * sizeof(*current), 2);
+
 		rf12_sleep(RF12_SLEEP);
+		activity_led.digiWrite(0);
+
+		memcpy(&old, current, 3 * sizeof(*current));
 	}
 }
