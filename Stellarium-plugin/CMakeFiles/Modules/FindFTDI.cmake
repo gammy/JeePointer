fa592dd128061f847a04621cc50e2553e6025880
gammy <gammy@denice>
2012-05-02 11:04:51 +0100

    More cmake cleanups for FTDI

--- Stellarium-plugin/CMakeFiles/Modules/FindFTDI.cmake
+++ Stellarium-plugin/CMakeFiles/Modules/FindFTDI.cmake
@@ -1,46 +1,29 @@
-# Try to find ftdi
-# Once done this will define
-#
-#  FTDI_FOUND - system has ftdi
-#  FTDI_INCLUDE_DIR - ~ the ftdi include directory 
-#  FTDI_LIBRARY - Link these to use ftdi
-#
-#  2010, Ze Ji
-#
+MESSAGE(STATUS "Looking for ftdi")
+
 IF(WIN32)
-	FIND_PATH(FTDI_INCLUDE_DIR 
-	NAMES   ftdi.h
-	PATHS   $ENV{FTDI_DIR}/src
-			"C:/roboskin/software_yarp_related/ftdi_usb/libftdi-0.18/src"
-	)
-
-	FIND_LIBRARY(FTDI_LIBRARY
-	NAMES ftdi
-	PATHS $ENV{FTDI_DIR}/src/Debug
-		  "C:/roboskin/software_yarp_related/ftdi_usb/libftdi-0.18/src/Debug"
-		  $ENV{FTDI_DIR}/src/Release
-		  "C:/roboskin/software_yarp_related/ftdi_usb/libftdi-0.18/src/Release"
-	)
-						   
+	MESSAGE(STATUS "Eep, no configuration for windows")
 ELSE(WIN32)  
-	FIND_PATH(FTDI_INCLUDE_DIR NAMES ftdi.hpp
-		PATHS	/usr/local/include
-		/usr/include
-		/usr/include/libftdi
-		/usr/local/include/libftdi
+	FIND_PATH(FTDI_INCLUDE_DIR 
+		NAMES ftdi.hpp
+		PATHS /usr/local/include
+		      /usr/include
+		      /usr/include/libftdi
+		      /usr/local/include/libftdi
 	)
 
-	FIND_LIBRARY(FTDI_LIBRARY NAMES ftdipp
-		PATHS	/usr/lib64
-		/usr/lib
-		/usr/local/lib
-		/usr/local/lib64
+	FIND_LIBRARY(FTDI_LIBRARY 
+		NAMES ftdipp
+		PATHS /usr/lib64
+		      /usr/local/lib64
+		      /usr/lib
+		      /usr/local/lib
 	)
 ENDIF(WIN32)
 
 
 IF (FTDI_LIBRARY)
     set(FTDI_FOUND TRUE)
+    MESSAGE(STATUS "Looking for ftdi - found")
 ELSE (FTDI_LIBRARY)
     set(FTDI_FOUND FALSE)    
 ENDIF (FTDI_LIBRARY)
@@ -48,5 +31,3 @@ ENDIF (FTDI_LIBRARY)
 set(FTDI_INCLUDE_DIR
     ${FTDI_INCLUDE_DIR}
 )
-
-#MESSAGE("----------ftdi: ${FTDI_INCLUDE_DIR} ftdilib: ${FTDI_LIBRARY}")
